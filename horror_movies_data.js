ccab1c56b36b6a185e91ee6f67cd0eadc24cec30
John <jb7452898@gmail.com>
2026-09-05 04:42:47 -0700

    adding horror movie match game along with its current data

---
+++ horror_movies_data.js
@@ -0,0 +1,122 @@
+const SUBJECT_DATA = [
+  {
+    "first": "the exorcist",
+    "second": "wiliam peter blatty",
+    "third": "max von sydow",
+    "fourth": "possession"
+  },
+  {
+    "first": "a nightmare on elm street",
+    "second": "freddy kruger",
+    "third": "johnny depp",
+    "fourth": "robert englund"
+  },
+  {
+    "first": "a nightmare on elm street",
+    "second": "heather langenkamp",
+    "third": "john saxon",
+    "fourth": "amanda wyss"
+  },
+  {
+    "first": "the shining",
+    "second": "jack nicholson",
+    "third": "shelly duvall",
+    "fourth": "stephen king"
+  },
+  {
+    "first": "the shining",
+    "second": "colorado",
+    "third": "overlook hotel",
+    "fourth": "hedge maze"
+  },
+  {
+    "first": "the shining",
+    "second": "writer",
+    "third": "alcoholic",
+    "fourth": "stanley kubrick"
+  },
+  {
+    "first": "the thing",
+    "second": "john carpenter",
+    "third": "antarctica",
+    "fourth": "alien creature"
+  },
+  {
+    "first": "the omen",
+    "second": "richard donner",
+    "third": "damien thorn",
+    "fourth": "lee remick"
+  },
+  {
+    "first": "the people under the stairs",
+    "second": "wes craven",
+    "third": "ving rhames",
+    "fourth": "wendy robie"
+  },
+  {
+    "first": "hellraiser",
+    "second": "clive barker",
+    "third": "pinhead",
+    "fourth": "cenobites"
+  },
+  {
+    "first": "candyman",
+    "second": "bernard rose",
+    "third": "virginia madsen",
+    "fourth": "clive barker"
+  },
+  {
+    "first": "the thing",
+    "second": "john carpenter",
+    "third": "antarctica",
+    "fourth": "kurt russell"
+  },
+  {
+    "first": "bram stokers dracula",
+    "second": "anthony hopkins",
+    "third": "keanu reeves",
+    "fourth": "francis ford coppola"
+  },
+  {
+    "first": "bram stokers dracula",
+    "second": "gary oldman",
+    "third": "winona ryder",
+    "fourth": "abraham van helsing"
+  },
+  {
+    "first": "i know what you did last summer",
+    "second": "jennifer love hewitt",
+    "third": "sarah michelle gellar",
+    "fourth": "ryan phillippe"
+  },
+  {
+    "first": "scream",
+    "second": "drew barrymore",
+    "third": "wes craven",
+    "fourth": "neve campbell"
+  },
+  {
+    "first": "the craft",
+    "second": "fairuza balk",
+    "third": "rachel true",
+    "fourth": "neve campbell"
+  },
+  {
+    "first": "leprechaun",
+    "second": "jennifer aniston",
+    "third": "mark jones",
+    "fourth": "dan o grady returns from ireland"
+  },
+  {
+    "first": "tremors",
+    "second": "kevin bacon",
+    "third": "fred ward",
+    "fourth": "reba mcentire"
+  },
+  {
+    "first": "blade",
+    "second": "wesley snipes",
+    "third": "marvel",
+    "fourth": "david goyer"
+  }
+];
